7e1dae812620057fe156ab2bd11b39bfd6977d8f
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2018-08-16 11:47:32 -0300

    Styling locale selector.

--- src/components/ChangeLanguage.js
+++ src/components/ChangeLanguage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {inject, observer} from 'mobx-react';
+import styled from 'styled-components';
 
 import Txt from './Txt';
 
@@ -9,20 +10,35 @@ import Txt from './Txt';
  */
 const ChangeLanguage = ({store}) => (
 	<div>
-		<Txt val="Language"/>
-		{['en', 'pt'].map((lang) =>
-			store.locale === lang ?
-				lang.toUpperCase() :
-				<button key={lang} onClick={e => store.locale = lang}>
-					{lang.toUpperCase()}
-				</button>
-		)}
+		<DivTitle><Txt val="Language"/></DivTitle>
+		<DivOpts>
+			{['en', 'pt'].map((lang) =>
+				store.locale === lang ?
+					<span>{lang.toUpperCase()}</span> :
+					<button key={lang} onClick={e => store.locale = lang}>
+						{lang.toUpperCase()}
+					</button>
+			)}
+		</DivOpts>
 	</div>
 );
 
 ChangeLanguage.propTypes = {
 	store: PropTypes.any.isRequired
 };
+
+const DivTitle = styled.div`
+	display: inline-block;
+	width: 100px;
+`;
+const DivOpts = styled.div`
+	display: inline-block;
+	border: 1px solid #ccc;
+	padding: 3px;
+	& > span, & > button {
+		margin: 0 5px;
+	}
+`;
 
 export default inject('store')(
 	observer(ChangeLanguage)
