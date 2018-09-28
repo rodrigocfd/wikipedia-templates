02af07beffec457ae99ca97553f334fcc590318c
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2018-09-28 11:42:17 -0300

    Intz accepting printf-like formatting.

--- src/components/Intz.js
+++ src/components/Intz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 
 const {Provider: ContextProvider, Consumer: ContextConsumer} = React.createContext();
@@ -21,15 +21,50 @@ IntzProvider.propTypes = {
 /**
  * Will translate one string according to current provider lang.
  */
-const Intz = ({str}) => (
+const Intz = ({str, args}) => (
 	<ContextConsumer>
-		{contextData => contextData.locales[contextData.lang][str] ?
-			contextData.locales[contextData.lang][str] : `[${str}]`}
+		{contextData => {
+			if (!contextData.locales[contextData.lang][str]) {
+				return `[${str}]`;
+			}
+
+			if (args) {
+				let retJsx = [];
+				let regex = new RegExp('\\{\\d+\\}', 'g');
+				let match = null;
+				let prevStartIdx = 0;
+				let curChildKey = 0;
+
+				while ((match = regex.exec(str)) !== null) {
+					let token = match[0];
+					let replacementIdx = Number(token.substr(1, token.length - 2));
+					let tokenIdx = match.index;
+					let prevStr = str.substr(prevStartIdx, tokenIdx - prevStartIdx);
+
+					if (prevStr.length) {
+						retJsx.push(<Fragment key={curChildKey++}>{prevStr}</Fragment>);
+					}
+
+					retJsx.push(<Fragment key={curChildKey++}>{args[replacementIdx]}</Fragment>);
+					prevStartIdx = tokenIdx + token.length;
+				}
+
+				let lastPart = str.substr(prevStartIdx);
+				if (lastPart.length) {
+					retJsx.push(<Fragment key={curChildKey++}>{lastPart}</Fragment>);
+				}
+
+				return retJsx;
+			}
+
+			return contextData.locales[contextData.lang][str];
+		}}
 	</ContextConsumer>
 );
 
 Intz.propTypes = {
-	str: PropTypes.string.isRequired
+	str: PropTypes.string.isRequired,
+	args: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.element]))
 };
 
 /**
