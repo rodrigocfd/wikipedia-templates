3a0babc8d3e42bf58f017cb71f3dee4a5f61a872
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2018-12-04 07:46:36 -0200

    Updates

---
+++ precache-manifest.46561642238cdb5e3c330074c8b41a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a767f410ea8d2c07d3d",
+    "url": "/wikipedia-templates/static/js/runtime~main.8a767f41.js"
+  },
+  {
+    "revision": "889abee0e67085a6b396",
+    "url": "/wikipedia-templates/static/js/main.889abee0.chunk.js"
+  },
+  {
+    "revision": "9ee47aea2e823869aa5d",
+    "url": "/wikipedia-templates/static/js/1.9ee47aea.chunk.js"
+  },
+  {
+    "revision": "889abee0e67085a6b396",
+    "url": "/wikipedia-templates/static/css/main.d80abe2b.chunk.css"
+  },
+  {
+    "revision": "9c4b9d8ae40af14f117397cb624c4417",
+    "url": "/wikipedia-templates/index.html"
+  }
+];
