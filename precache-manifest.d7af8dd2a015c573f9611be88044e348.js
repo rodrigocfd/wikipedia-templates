ae050fee1db0065d21ad748baf4361ed2e9c371b
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2019-01-14 09:39:08 -0200

    Updates

---
+++ precache-manifest.d7af8dd2a015c573f9611be88044e348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a767f410ea8d2c07d3d",
+    "url": "/wikipedia-templates/static/js/runtime~main.8a767f41.js"
+  },
+  {
+    "revision": "c472a49347d5db2a8107",
+    "url": "/wikipedia-templates/static/js/main.c472a493.chunk.js"
+  },
+  {
+    "revision": "948968f2dd418092bffb",
+    "url": "/wikipedia-templates/static/js/1.948968f2.chunk.js"
+  },
+  {
+    "revision": "c472a49347d5db2a8107",
+    "url": "/wikipedia-templates/static/css/main.46521d13.chunk.css"
+  },
+  {
+    "revision": "46cb5f56e46a31eceb24463d2a012a5a",
+    "url": "/wikipedia-templates/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wikipedia-templates/precache-manifest.8076e797f7d3df150c5b6979d35e8103.js"
+  "/wikipedia-templates/precache-manifest.d7af8dd2a015c573f9611be88044e348.js"
 );
 
 workbox.clientsClaim();
