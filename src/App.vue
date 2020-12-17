53b91bbc9a391771ca99d12ce6e95f57f0c73866
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-12-17 17:47:59 -0300

    First fields in CSS grid.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png">
-	<HelloWorld msg="Bango" @clique="clicou" />
+	<CiteWeb />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CiteWeb from './components/CiteWeb.vue';
 
 export default {
-	components: {HelloWorld},
+	components: {CiteWeb},
 
 	setup() {
-		function clicou(name) {
-			alert(`Parent received [${name}]`);
-		}
-
-		return {
-			clicou,
-		};
 	}
 };
 </script>
@@ -26,8 +18,6 @@ export default {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
 }
 </style>
---
+++ src/components/CiteWeb.vue
@@ -0,0 +1,45 @@
+<template>
+	<h1>Cite web</h1>
+	<div>
+		<div class="fields">
+			URL <input type="text" v-model="url" />
+			Title <input type="text" v-model="title" />
+		</div>
+	</div>
+	<textarea :value="tag" />
+</template>
+
+<script>
+import {computed, ref} from 'vue';
+
+export default {
+	setup() {
+		const url = ref('');
+		const title = ref('');
+
+		const tag = computed(() =>
+			'{{cite web' +
+			(url.value ? ` |url=${url.value}` : '') +
+			(title.value ? ` |url=${title.value}` : '') +
+			'}}'
+		);
+
+		return {
+			url,
+			tag,
+		};
+	}
+};
+</script>
+
+<style scoped>
+.fields {
+	display: inline-grid;
+	grid-template-columns: auto auto;
+	margin-bottom: 20px;
+}
+textarea {
+	width: 800px;
+	height: 60px;
+}
+</style>
