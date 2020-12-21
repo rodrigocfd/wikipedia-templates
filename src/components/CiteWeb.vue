812e2233ed6fea03be94b534d7960a030ef5e4a3
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-12-21 16:16:36 -0300

    Author fields.

--- src/components/CiteWeb.vue
+++ src/components/CiteWeb.vue
@@ -4,6 +4,11 @@
 		<div class="fields">
 			<label>URL</label><input type="text" v-model="url" />
 			<label>Title</label><input type="text" v-model="title" />
+			<label>Author</label>
+			<div>
+				<label>Last name</label><input type="text" v-model="last" />
+				<label>First name</label><input type="text" v-model="first" />
+			</div>
 		</div>
 	</div>
 	<textarea :value="result" />
@@ -16,15 +21,19 @@ export default {
 	setup() {
 		const url = ref('');
 		const title = ref('');
+		const last = ref('');
+		const first = ref('');
 
 		const result = computed(() =>
 			'{{cite web' +
 			(url.value ? ` |url=${url.value}` : '') +
 			(title.value ? ` |title=${title.value}` : '') +
+			(last.value ? ` |last=${last.value}` : '') +
+			(first.value ? ` |last=${first.value}` : '') +
 			'}}'
 		);
 
-		return {url, title, result};
+		return {url, title, last, first, result};
 	}
 };
 </script>
@@ -33,13 +42,17 @@ export default {
 .fields {
 	display: inline-grid;
 	grid-template-columns: auto auto;
-	grid-gap: 12px;
+	grid-gap: 12px 0;
 	margin-bottom: 20px;
 	& > label {
 		align-self: center;
+		margin-right: 12px;
+	}
+	& > div > label {
+		margin: 0 12px;
 	}
 	& > input {
-		width: 400px;
+		width: 500px;
 		font-family: monospace;
 	}
 }
