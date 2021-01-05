c37f4a0d07193975ae776f30ec7e03257106ccc2
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-01-05 10:31:25 -0300

    Production base URL in router.

--- src/router.js
+++ src/router.js
@@ -5,6 +5,9 @@ import NotFound from '@/pages/NotFound';
 
 export default createRouter({
 	history: createWebHistory(),
+	base: process.env.NODE_ENV === 'production' // follows vue.config.js
+		? '/wikipedia-templates/'
+		: '/',
 	routes: [
 		{ path: '/', redirect: '/cite-web' },
 		{ path: '/cite-web', component: CiteWeb },
